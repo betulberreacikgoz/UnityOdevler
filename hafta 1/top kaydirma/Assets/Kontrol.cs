5e8de484ea69b5a1e9a26c98f36428b8609095ad
betulacikgoz <153846808+betulacikgoz@users.noreply.github.com>
2025-02-24 23:18:24 +0300

    Hafta 1

---
+++ hafta 1/top kaydirma/Assets/Kontrol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ);
+        rb.AddForce(movement * moveSpeed, ForceMode.Acceleration);
+    }
+}
